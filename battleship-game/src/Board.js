acd6713acdd889b16cee9e0fb47a718cf71ae11b
learn <learn@learn06-2.local>
2018-01-23 09:10:15 -0800

    updated board

--- battleship-game/src/Board.js
+++ battleship-game/src/Board.js
@@ -10,46 +10,66 @@ class Board extends Component { //begin class
       board: [] //sets board to an empty array
     }
 
-    //begins function for onclick change
-    // clickHandler(x,y){
-    //   this.setState({
-    //     newBoard: this.state.board //changes the board for what is clicked
-    //   })
+    // //begins function for onclick change
+    // // clickHandler(x,y){
+    // //   this.setState({
+    // //     newBoard: this.state.board //changes the board for what is clicked
+    // //   })
     // }
+
     this.setUpBoard()
     this.fiveShips()
   }
     setUpBoard(){ //empty array inside of board
-      for(var i = 0; i < 10; i++){
-        this.state.board.push([])
+      let board = []
+
+      for(var row = 0; row < 10; row++){
+        board[row] = []
+
+        for(var col = 0; col < 10; col++) {
+          board[row][col] = 0
+        }
       }
+      console.log(board);
+      this.state.board = board
     }
+
     placeShips() { //will randomly place ships on the grid
-      var x = Math.floor(Math.random() * 10)
-      var y = Math.floor(Math.random() * 10)
-      this.state.board[x][y]=ship
+      var row = Math.floor(Math.random() * 10)
+      var col = Math.floor(Math.random() * 10)
+
+      this.state.board[row][col]=ship
     }
-    fiveShips(){ //places five random ships on the board
+
+    fiveShips() { //places five random ships on the board
       for(var i = 0; i<5; i++){
         this.placeShips()
       }
     }
 
  renderRow(rowNumber){ //creates one row
+   // TODO: rename vars
     var row = []
     for(var i = 0; i < 10; i++){
-      row.push(<td id="grid"></td>)
+      if(this.state.board[rowNumber][i]==ship){
+          row.push(<td id="grid">{ship}</td>)
+      } else {
+        row.push(<td id="grid">{}</td>)
+      }
+
 
     }
     return row
   }
+
   renderRows(){ //creates ten rows
     var rows = []
     for(var i = 0; i < 10; i++){
-      rows.push(<tr id="row">{this.renderRow()}</tr>)
+      rows.push(<tr id="row">{this.renderRow(i)}</tr>)
     }
     return rows
   }
+
   render(){ //renders our grid
     return(
       <table>
